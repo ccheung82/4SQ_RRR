5d6dc5b8bca66e2f5e32cbc79f87dfb00bfe2da9
Calvin Cheung <calvin.cheung82@gmail.com>
2020-02-11 00:11:07 -0500

    Prototype Build
    Added Room and table
    Gave them materials
    Added food place holder prefab
    Made Launching Script
    Added Pause Menu

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Back2Title()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Pressed");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject food;
+    public int speed = 9;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            Debug.Log("Spawn Button pressed");
+            GameObject newFood = Instantiate(food);
+            newFood.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        }
+    }
+}
