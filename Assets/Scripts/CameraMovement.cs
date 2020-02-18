9cb66cc2cd8b26aa0e26925f2969eb037824feb3
izzyc323 <castroi@ufl.edu>
2020-02-18 16:08:03 -0500

    Isabel Prototype Assets
    Brigning in all assets

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+ public float Speed = 10.0f;
+
+    // Vector3 right = new Vector3(0, 90, 0);
+    // Vector3 left = new Vector3(0, -90, 0);
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.AngleAxis(90, transform.up) * transform.rotation;
+            //transform.Rotate(right * Time.deltaTime * Speed);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.AngleAxis(-90, transform.up) * transform.rotation;
+            //transform.Rotate(left * Time.deltaTime * Speed);
+
+        }
+
+        //transform.rotation = vector;
+        //transform.position = Vector3.Lerp(transform.position, currentPos, Speed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/foodMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodMove : MonoBehaviour
+{
+    public float speed = 2.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
