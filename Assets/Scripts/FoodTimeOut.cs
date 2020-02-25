c1e4c2a1b9005c18c0f211a7332cbd68c05e3224
izzyc323 <castroi@ufl.edu>
2020-02-25 16:46:51 -0500

    food time out script
    food will get destroyed if it touches a character or if it has been alive for 5 seconds

---
+++ Assets/Scripts/FoodTimeOut.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script will be in every food object and will destory the food if it hasn't already been in 5 seconds
+
+public class FoodTimeOut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(WaitAndDestroy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator WaitAndDestroy()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("colllision detected");
+
+        if (col.gameObject.tag == "customer")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
