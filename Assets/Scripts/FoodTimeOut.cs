9618fe584c0d0cf023070e1463be6cd12efe2712
izzyc323 <castroi@ufl.edu>
2020-03-17 16:31:56 -0400

    FoodTimeOut fixed
    now food that doesn't get eaten times out after 5 seconds

--- Assets/Scripts/FoodTimeOut.cs
+++ Assets/Scripts/FoodTimeOut.cs
@@ -10,7 +10,7 @@ public class FoodTimeOut : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(WaitAndDestroy());
+        StartCoroutine(WaitAndDestroy());
        
     }
 
