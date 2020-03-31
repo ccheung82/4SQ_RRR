ec805347c37e3b21377de77e4c9473676ee5a0e5
jjmorales <jonjoemorales@gmail.com>
2020-03-31 16:55:19 -0400

    game over bug fixed on timer strike

--- Assets/Scripts/timerScript.cs
+++ Assets/Scripts/timerScript.cs
@@ -38,6 +38,10 @@ public class timerScript : MonoBehaviour
             timer = 0;
 
             GameObject.FindWithTag("scoreSystem").GetComponent<Score>().addStrike();
+
+            if(GameObject.FindWithTag("scoreSystem").GetComponent<Score>().isGameOver()){
+                UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+            }
         }
 
         if(resetBool) {
