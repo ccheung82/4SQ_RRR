aebd5873927281891eb841dff5469a7215055769
ml-designer <matthewjleonard@ufl.edu>
2020-04-19 11:14:51 -0400

    Sound Files For Scenes
    Still need to add interaction sounds for animals in the scripts

---
+++ Assets/4Sq Sounds/MusicClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
---
+++ Assets/4Sq Sounds/NotPlaying.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotPlaying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>().StopMusic();
+    }
+}
