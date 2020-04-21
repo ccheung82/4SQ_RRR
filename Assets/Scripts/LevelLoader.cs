c90339c041d237607cba4c1bce007d9fedd9e295
Mateo Delgadillo <matd97@gmail.com>
2020-04-21 12:23:28 -0400

    added splashscreen

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,58 +8,57 @@ public class LevelLoader : MonoBehaviour
     [SerializeField] public Animator transition;
     private int levelToLoad;
 
-    public void loadStart()
+    public void loadSplash()
     {
         levelToLoad = 0;
         transition.SetTrigger("Start");
     }
 
-    public void loadInstructions()
+    public void loadStart()
     {
         levelToLoad = 1;
         transition.SetTrigger("Start");
     }
 
-    public void loadLevelSelect()
+    public void loadInstructions()
     {
         levelToLoad = 2;
         transition.SetTrigger("Start");
     }
 
-    public void loadEasy()
+    public void loadLevelSelect()
     {
         levelToLoad = 3;
         transition.SetTrigger("Start");
     }
 
-    public void loadMedium()
+    public void loadEasy()
     {
         levelToLoad = 4;
         transition.SetTrigger("Start");
     }
 
-    public void loadHard()
+    public void loadMedium()
     {
         levelToLoad = 5;
         transition.SetTrigger("Start");
     }
 
-    public void loadGameOver()
+    public void loadHard()
     {
         levelToLoad = 6;
         transition.SetTrigger("Start");
     }
 
-    public void loadSettings()
+    public void loadGameOver()
     {
         levelToLoad = 7;
         transition.SetTrigger("Start");
-        Debug.Log("IMPLEMENT SETTINGS SCREEN");
     }
 
     public void loadCredits()
     {
-        levelToLoad = 7;
+        levelToLoad = 8;
         transition.SetTrigger("Start");
         Debug.Log("IMPLEMENT CREDITS SCREEN");
     }
@@ -69,15 +68,4 @@ public class LevelLoader : MonoBehaviour
         //load scene
         SceneManager.LoadScene(levelToLoad);
     }
-
-
-
-
-    //IEnumerator LoadLevel(int levelIndex)
-    //{
-    //    //play animation
-    //    transition.SetTrigger("Start");
-    //    //wait
-    //    yield return new WaitForSeconds(transitionTime);
-    //}
 }
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,14 +7,12 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     public GameObject playButton;
-    public GameObject settingsButton;
     public GameObject creditsButton;
     [SerializeField] LevelLoader levLoader;
 
     void Start()
     {
         playButton.SetActive(true);
-        settingsButton.SetActive(true);
     }
 
     public void PlayButtonClicked()
